982981eea79784dd847ccc6ab79a6921918e64da
Kong Jin Jie <jinjie@swiftdev.sg>
2018-08-14 23:39:02 +0800

    First import

---
+++ README.md
@@ -0,0 +1,3 @@
+# SilverStripe 4 Duplicate DataObject
+
+Adds a duplicate button to GridField in the CMS that enables duplicating of dataobjects
---
+++ src/Forms/GridField/GridFieldDuplicateAction.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace SwiftDev\DuplicateDataObject\Forms\GridField;
+
+use SilverStripe\Control\Controller;
+use SilverStripe\Forms\GridField\GridField;
+use SilverStripe\Forms\GridField\GridField_ActionProvider;
+use SilverStripe\Forms\GridField\GridField_ColumnProvider;
+use SilverStripe\Forms\GridField\GridField_FormAction;
+
+class GridFieldDuplicateAction 
+    implements 
+        GridField_ColumnProvider,
+        GridField_ActionProvider
+{
+
+    public function augmentColumns($gridField, &$columns)
+    {
+        if (! in_array('Actions', $columns))
+        {
+            $columns[] = 'Actions';
+        }
+    }
+
+    public function getColumnAttributes($gridField, $record, $columnNamme)
+    {
+        return ['class' => 'grid-field__col-compact'];
+    }
+
+    public function getColumnMetadata($gridField, $columnName) 
+    {
+        if ($columnName == 'Actions')
+        {
+            return ['title' => ''];
+        }
+    }
+
+    public function getColumnsHandled($gridField) 
+    {
+        return ['Actions'];
+    }
+
+    public function getColumnContent($gridField, $record, $columnName) 
+    {
+        if(!$record->canEdit()) return;
+
+        $field = GridField_FormAction::create(
+            $gridField,
+            'DuplicateAction'.$record->ID,
+            false,
+            "duplicateobject",
+            ['RecordID' => $record->ID]
+        )
+            ->addExtraClass('gridfield-button-duplicate btn--icon-md font-icon-page-multiple btn--no-text grid-field__icon-action')
+            ->setAttribute('title', 'Duplicate ' . $record->singular_name())
+            ->setDescription('Duplicate ' . $record->singular_name());
+
+        return $field->Field();
+    }
+
+    public function getActions($gridField) 
+    {
+        return ['duplicateobject'];
+    }
+
+    public function handleAction(GridField $gridField, $actionName, $arguments, $data) 
+    {
+        if($actionName == 'duplicateobject') {
+            $item = $gridField->getList()->byID($arguments['RecordID']);
+
+            $clone = $item->duplicate();
+
+            Controller::curr()->getResponse()->setStatusCode(
+                200,
+                "{$item->Title} Duplicated"
+            );
+        }
+    }
+}
